ebc6591d97a1a51895f53d5b264a9c1c0c562d5d
Sheikh Kohinoor Rahman <160453453+Kohin-Rahman@users.noreply.github.com>
2026-08-29 13:50:37 +0600

    Add student tuples and print statements

---
+++ Tuples.py
@@ -0,0 +1,17 @@
+students = (
+    "Sheikh Mujibur Rahman",
+    "Nur Nahar Begum",
+    "Nusrat Najuman Sany",
+    "Israt Najuman Shanta",
+    ("Sheikh Kohinoor Rahman Dipu",23,3.95)
+)
+
+print(students,"\n")
+
+print(students[0],"\n")
+
+print(students[4],"\n")
+
+print(students[4][2],"\n")
+
+print(students[1:])
